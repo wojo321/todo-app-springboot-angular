4e27e4ade1e012518e4a7fecfd38a0a9987a9197
wojo321 <wojtek.f.99@gmail.com>
2022-04-13 23:56:04 +0200

    Created project structure

---
+++ frontend/todo-app/src/app/all-tasks-view/all-tasks-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-all-tasks-view',
+  templateUrl: './all-tasks-view.component.html',
+  styleUrls: ['./all-tasks-view.component.css']
+})
+export class AllTasksViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
